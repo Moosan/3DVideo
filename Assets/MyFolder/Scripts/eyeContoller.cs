3d8c19f2d465ca0ff95669dbefac3634b06d2c08
Moosan <washer2016@outlook.jp>
2018-03-07 18:50:50 +0900

    InputFieldnotyousi

--- Assets/MyFolder/Scripts/eyeContoller.cs
+++ Assets/MyFolder/Scripts/eyeContoller.cs
@@ -15,20 +15,39 @@ public class eyeContoller : MonoBehaviour {
     private GameObject LeftCamera;
 
 	void Start () {
-        kyo.OnValueChangedAsObservable().Subscribe(value => sisaChange(Parse(value)));
-        kak.OnValueChangedAsObservable().Subscribe(value => kakuChange(Parse(value)));
+        kyo.OnValueChangedAsObservable().Subscribe(value =>OnKyoriValueChange(value));
+        kak.OnValueChangedAsObservable().Subscribe(value => OnKakuValueChange(value));
+        kyo.OnEndEditAsObservable().Subscribe(value => sisaChange(Parse(value)));
+        kak.OnEndEditAsObservable().Subscribe(value => kakuChange(Parse(value)));
         kyori.OnValueChangedAsObservable().Subscribe(value => sisaChange(value));
         kaku.OnValueChangedAsObservable().Subscribe(value => kakuChange(value));
     }
-    private void sisaChange(float sisa) {
+    private void sisaChange(string sisa) {
+        kyo.text = sisa;
+        var a = float.Parse(sisa);
+        RightCamera.transform.localPosition = new Vector3(a, 0, 0);
+        LeftCamera.transform.localPosition = new Vector3(-1 * a, 0, 0);
+    }
+    private void sisaChange(float sisa)
+    {
+        kyo.text = sisa.ToString();
         RightCamera.transform.localPosition = new Vector3(sisa, 0, 0);
         LeftCamera.transform.localPosition = new Vector3(-1 * sisa, 0, 0);
-        kyo.text = sisa.ToString();
     }
-    private void kakuChange(float kaku) {
+
+    private void kakuChange(string kaku) {
+        kak.text = kaku.ToString();
+        var a = float.Parse(kaku);
+        RightCamera.transform.rotation = Quaternion.Euler(new Vector3(transform.eulerAngles.x, -1 * a, 0));
+        LeftCamera.transform.rotation = Quaternion.Euler(new Vector3(transform.eulerAngles.x, a, 0));
+        
+    }
+    private void kakuChange(float kaku)
+    {
+        kak.text = kaku.ToString();
         RightCamera.transform.rotation = Quaternion.Euler(new Vector3(transform.eulerAngles.x, -1 * kaku, 0));
         LeftCamera.transform.rotation = Quaternion.Euler(new Vector3(transform.eulerAngles.x, kaku, 0));
-        kak.text = kaku.ToString();
+
     }
     private float Floor(float value) {
         return (Mathf.Round(1000 * value) / 1000f);
@@ -42,4 +61,27 @@ public class eyeContoller : MonoBehaviour {
             return float.Parse(value);
         }
     }
+    private void OnKyoriValueChange(string value) {
+        sisaChange(ValueParse(value));
+    }
+    private void OnKakuValueChange(string value)
+    {
+        kakuChange(ValueParse(value));
+    }
+    private string ValueParse(string value) {
+        float a;
+        if (string.IsNullOrEmpty(value)) {
+            return "0";
+        }
+        if (float.TryParse(value, out a))
+        {
+            var array = value.ToCharArray();
+            if (array.Length > 5) {
+                return Floor(a).ToString();
+            }
+            return value;
+        }
+        return "0";
+    }
+    
 }
